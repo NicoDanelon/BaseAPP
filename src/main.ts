d7ab98ba2b7cb944f857d71b265eff79c52a1b9c
nicodanelon <nicodanelon@outlook.com>
2023-06-04 20:18:55 -0300

    arquitectura de soft completa

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
 /// <reference types="@angular/localize" />
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
-
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, NgbModule),
-        provideAnimations()
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(BrowserModule, NgbModule),
+    provideAnimations(),
+  ],
+}).catch((err) => console.error(err));
