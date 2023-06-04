b7df2ee4d571a6563c3e1fad0f4888ccd740135b
nicodanelon <nicodanelon@outlook.com>
2023-06-04 19:47:38 -0300

    instalaciones para el inicio del proyecto realizadas

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
+/// <reference types="@angular/localize" />
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, NgbModule),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
