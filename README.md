3e560fd035986a3398ffb5c2f921984a63281624
nicodanelon <nicodanelon@outlook.com>
2023-06-12 10:40:29 -0300

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Parentesis
+# Kioskito
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.2.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,9 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [AppComponent]
-}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [AppComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Parentesis'`, () => {
+  it(`should have as title 'Kioskito'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Parentesis');
+    expect(app.title).toEqual('Kioskito');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Parentesis app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'Kioskito app is running!'
+    );
   });
 });
